7eff5f9ce8fb8b38f5825a164ec48dc397840db5
minarin0179 <55317109+minarin0179@users.noreply.github.com>
2024-06-29 01:12:00 +0900

    feature-pollから移植

---
+++ src/components/buttons/agregatePoll.ts
@@ -0,0 +1,97 @@
+import { ButtonBuilder, ButtonStyle, Collection, EmbedBuilder } from "discord.js";
+import { Button } from "../../structures/Button";
+import { PollModel } from "../../structures/Poll";
+import { reply } from "../../utils/Reply";
+import unehemeralButton from "../../components/buttons/unehemeral";
+import revoteButton from "./revote";
+import { buttonToRow } from "../../utils/ButtonToRow";
+import { rename } from "../../commands/slashcommands/rename";
+
+export default new Button({
+    customId: "agregatePoll",
+    build: ({ pollId }: { pollId: string }) => [
+        new ButtonBuilder().setCustomId(`agregatePoll:${pollId}`).setLabel("集計").setStyle(ButtonStyle.Danger),
+    ],
+    execute: async ({ interaction, args }) => {
+        const pollId = args[0];
+        const poll = await PollModel.findById(pollId);
+        if (!poll) return;
+
+        if (interaction.user.id !== poll.ownerId) {
+            await reply(interaction, { content: "集計は投票を開始した人のみが行えます", ephemeral: true });
+            return;
+        }
+
+        const votes = poll.voters; // Map<userID, choiceIndex> 誰が何に入れたか
+        const result = new Collection<number, string[]>(); // Map<choiceIndex, [userID]> 何に誰が入れたか
+        votes.forEach((choiceIndex, userId) => {
+            if (!result.has(choiceIndex)) result.set(choiceIndex, []);
+            result.get(choiceIndex)?.push(userId);
+        });
+        const guild = interaction.guild!;
+
+        if (poll.type === "char") {
+            let warn = false;
+            const content = [...votes.entries()]
+                .map(([userId, choiceIndex]) => {
+                    const choice = poll.choices[choiceIndex];
+                    const { roleId } = choice;
+                    const role = roleId ? guild.roles.cache.get(roleId) : undefined;
+                    const member = guild.members.cache.get(userId);
+                    const roleName = role ? role.name : choice.label;
+                    if (result.get(choiceIndex)?.length === 1) {
+                        if (member) {
+                            rename(member, roleName).catch(() => { });
+                        }
+                        return `<@${userId}> → ${roleName} ✅`;
+                    } else {
+                        warn = true;
+                        return `<@${userId}> → ${roleName} ⚠️`;
+                    }
+                })
+                .join("\n");
+
+            await interaction.channel?.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .addFields({ name: "集計結果", value: content || "なし" })
+                        .setColor(warn ? 0xffcc4d : 0x77b255),
+                ],
+                allowedMentions: { parse: [] },
+                components: warn ? buttonToRow(revoteButton.build({ pollId })) : [],
+            });
+        } else if (poll.type === "vote") {
+            result.sort((a, b) => b.length - a.length);
+
+            const numOfVotes = result
+                .map((voters, choiceIndex) => {
+                    const choice = poll.choices[choiceIndex];
+                    return `${choice.label} : ${voters.length}票`;
+                })
+                .join("\n");
+
+            const votings = [...votes.entries()]
+                .map(([userId, choiceIndex]) => {
+                    const choice = poll.choices[choiceIndex];
+                    return `<@${userId}> → ${choice.label}`;
+                })
+                .join("\n");
+
+            await reply(interaction, {
+                embeds: [
+                    new EmbedBuilder()
+                        .addFields(
+                            { name: "投票数", value: numOfVotes || "なし" },
+                            { name: "投票先", value: votings || "なし" }
+                        )
+                        .setColor(0x3b88c3),
+                ],
+                allowedMentions: { parse: [] },
+                components: buttonToRow(unehemeralButton.build()),
+            });
+        }
+        await interaction.message.delete();
+
+        await reply(interaction, "集計結果を表示しました");
+    },
+});
---
+++ src/components/buttons/revote.ts
@@ -0,0 +1,43 @@
+import { ButtonBuilder, ButtonStyle, Collection } from "discord.js";
+import { Button } from "../../structures/Button";
+import { PollModel } from "../../structures/Poll";
+import { reply } from "../../utils/Reply";
+import { sendPoll } from "../../commands/slashcommands/poll";
+
+export default new Button({
+    customId: "revote",
+    build: ({ pollId }: { pollId: string }) => [
+        new ButtonBuilder().setCustomId(`revote:${pollId}`).setLabel("再投票").setStyle(ButtonStyle.Primary),
+    ],
+    execute: async ({ interaction, args }) => {
+        const pollId = args[0];
+        const poll = await PollModel.findById(pollId);
+        if (!poll) return;
+
+        if (interaction.user.id !== poll.ownerId) {
+            await reply(interaction, { content: "再投票は投票を開始した人のみが行えます", ephemeral: true });
+            return;
+        }
+
+        const votes = poll.voters; // Map<userID, choiceIndex> 誰が何に入れたか
+        const result = new Collection<number, string[]>(); // Map<choiceIndex, [userID]> 何に誰が入れたか
+        votes.forEach((choiceIndex: number, userId: string) => {
+            if (!result.has(choiceIndex)) result.set(choiceIndex, []);
+            result.get(choiceIndex)?.push(userId);
+        });
+
+        // 投票数がでない選択しのみを取り出す
+        const restChoices = poll.choices.filter((_, index: number) => result.get(index)?.length !== 1);
+
+        const pollOptions = {
+            type: poll.type,
+            ownerId: poll.ownerId,
+            choices: restChoices,
+            voters: new Map(),
+        };
+
+        await sendPoll(interaction, pollOptions);
+
+        await reply(interaction, { content: "再投票を開始しました", ephemeral: true });
+    },
+});
