f72c64943a5c3183c48cb34d60ad1ff09f1b9d31
minarin0179 <55317109+minarin0179@users.noreply.github.com>
2024-08-02 00:15:21 +0900

    Revert "Merge pull request #51 from minarin0179/minarin0179-patch-1"
    This reverts commit 8335aaec320e5e22b3b23a7e7a2d4489150da0bd, reversing
    changes made to 89e89dd2f4f58e5614bec2085ab9dcb5592c9f2c.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { ShardingManager } from "discord.js";
 import "dotenv/config";
-import { generateHeapSnapshot } from "bun";
-import fs from "fs";
 
 //sharding typescript file
 
@@ -10,17 +8,6 @@ const manager = new ShardingManager("./src/bot.ts", {
     respawn: true,
 });
 
-manager.on("shardCreate", shard => {
-    console.log(`Launched shard ${shard.id}`);
-    setInterval(
-        () => {
-            const filename = `heap-shard-${shard.id}-${Date.now()}.json`;
-            const snapshot = generateHeapSnapshot();
-            console.log(`Writing heap snapshot to ${filename}`);
-            fs.writeFileSync(filename, JSON.stringify(snapshot, null, 2));
-        },
-        60 * 60 * 1000
-    );
-});
+manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
 
 manager.spawn({ timeout: 60000 });
