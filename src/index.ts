a263a0daf8cad4ddfbc829ca297df644ce578ac2
minarin0179 <55317109+minarin0179@users.noreply.github.com>
2024-08-03 02:39:00 +0900

    メモリーの消費量を出力

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,20 @@ const manager = new ShardingManager("./src/bot.ts", {
     respawn: true,
 });
 
-manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
+manager.on("shardCreate", shard => {
+    console.log(`Launched shard ${shard.id}`);
+
+    setInterval(
+        () => {
+            const used = process.memoryUsage();
+            const messages = [];
+            for (const key in used) {
+                messages.push(`${key}: ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`);
+            }
+            console.log(new Date(), `shardid: ${shard.id},`, messages.join(", "));
+        },
+        10 * 60 * 1000
+    );
+});
 
 manager.spawn({ timeout: 60000 });
