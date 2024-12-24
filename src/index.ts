13678d6b693e83040f359384611ccb7972eb3d5c
minarin0179 <55317109+minarin0179@users.noreply.github.com>
2024-12-24 19:14:52 +0900

    Revert "メモリーの消費量を出力"
    This reverts commit a263a0daf8cad4ddfbc829ca297df644ce578ac2.

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,6 @@ const manager = new ShardingManager("./src/bot.ts", {
     respawn: true,
 });
 
-manager.on("shardCreate", shard => {
-    console.log(`Launched shard ${shard.id}`);
-
-    setInterval(
-        () => {
-            const used = process.memoryUsage();
-            const messages = [];
-            for (const key in used) {
-                messages.push(`${key}: ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`);
-            }
-            console.log(new Date(), `shardid: ${shard.id},`, messages.join(", "));
-        },
-        10 * 60 * 1000
-    );
-});
+manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
 
 manager.spawn({ timeout: 60000 });
