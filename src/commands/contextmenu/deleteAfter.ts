c8943252ab1abee6f4cd0a8ad818e836218dbd46
minarin0179 <55317109+minarin0179@users.noreply.github.com>
2025-04-20 23:04:32 +0900

    Revert "hotfix: 荒らしに使われ売る機能を一時削除"
    This reverts commit 63dbd19944e349edf9554362d1ebd73ef9d8bbd2.

---
+++ src/commands/contextmenu/deleteAfter.ts
@@ -0,0 +1,26 @@
+import { ApplicationCommandType, ContextMenuCommandBuilder, Snowflake } from "discord.js";
+import { ContextMenu } from "../../structures/ContextMenu";
+import { reply } from "../../utils/Reply";
+import { deleteMultiMessages } from "../../utils/DeleteMultiMessages";
+import { fetchAllMessages } from "../../utils/FetchAllMessages";
+
+export default new ContextMenu({
+    data: new ContextMenuCommandBuilder()
+        .setName("これ以降を削除")
+        .setType(ApplicationCommandType.Message)
+        .setDefaultMemberPermissions(0)
+        .setDMPermission(false),
+    execute: async ({ interaction }) => {
+        if (!interaction.isMessageContextMenuCommand()) return;
+        await interaction.deferReply({ ephemeral: true });
+
+        const message = interaction.targetMessage;
+        const { channel } = message;
+        const after: Snowflake | undefined = message.id;
+
+        const messages = await fetchAllMessages(channel, { after });
+        await deleteMultiMessages(channel, messages);
+
+        await reply(interaction, "メッセージを削除しました");
+    },
+});
---
+++ src/commands/slashcommands/cleanup.ts
@@ -0,0 +1,52 @@
+import {
+    ChannelType,
+    GuildChannel,
+    GuildTextBasedChannel,
+    NewsChannel,
+    SlashCommandBuilder,
+    TextChannel,
+    VoiceChannel,
+} from "discord.js";
+import { SlashCommand } from "../../structures/SlashCommand";
+import { deleteMultiMessages } from "../../utils/DeleteMultiMessages";
+import { fetchAllMessages } from "../../utils/FetchAllMessages";
+import { isCategory } from "../../utils/isCategory";
+import { reply } from "../../utils/Reply";
+
+export default new SlashCommand({
+    data: new SlashCommandBuilder()
+        .setName("cleanup")
+        .setDescription("メッセージをすべて削除します")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(0)
+        .addChannelOption(option =>
+            option
+                .setName("対象")
+                .setDescription("指定しなかった場合はコマンドが送信されたチャンネル")
+                .setRequired(false)
+                .addChannelTypes(ChannelType.GuildText, ChannelType.GuildCategory)
+        ) as SlashCommandBuilder,
+
+    execute: async ({ interaction, args }) => {
+        await interaction.deferReply({ ephemeral: true });
+
+        const targetChannel = (args.getChannel("対象") ?? interaction.channel) as GuildChannel;
+        if (!targetChannel) return;
+
+        if (targetChannel.isTextBased()) {
+            await deleteAllMessages(targetChannel);
+        } else if (isCategory(targetChannel)) {
+            const channels = targetChannel.children.cache.filter(
+                (channel): channel is NewsChannel | TextChannel | VoiceChannel => channel.isTextBased()
+            );
+            await Promise.all(channels.map(channel => deleteAllMessages(channel)));
+        }
+
+        await reply(interaction, `「${targetChannel.name}」内のメッセージを削除しました`);
+    },
+});
+
+const deleteAllMessages = async (channel: GuildTextBasedChannel) => {
+    const messages = await fetchAllMessages(channel);
+    return deleteMultiMessages(channel, messages);
+};
